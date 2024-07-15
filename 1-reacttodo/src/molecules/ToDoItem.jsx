cd0f019d9e05c984f734aa6752bf151f75d84589
sabb12 <sabb0923@gmail.com>
2024-07-15 20:24:27 +0900

    added molecules folder: ToDoItem.jsx

---
+++ 1-reacttodo/src/molecules/ToDoItem.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import Button from "../atoms/Button";
+import Input from "../atoms/Input";
+import Checkbox from "../atoms/Checkbox";
+
+export default function TodoItem({
+  id,
+  text,
+  completed,
+  onUpdate,
+  onDelete,
+  onToggle,
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editText, setEditText] = useState(text);
+
+  return (
+    <div>
+      <Checkbox checked={completed} onChange={() => ontoggle(id)} />
+      {isEditing ? (
+        <Input value={editText} onChange={(e) => setEditText(e.target.value)} />
+      ) : (
+        <span>{text}</span>
+      )}
+      <Button onClick={() => onDelete(id)}>Delete</Button>
+      {!completed && (
+        <Button
+          onClick={() => {
+            if (isEditing) onUpdate(id, editText);
+            setIsEditing(!isEditing);
+          }}
+        >
+          {isEditing ? "Save" : "Edit"}
+        </Button>
+      )}
+    </div>
+  );
+}

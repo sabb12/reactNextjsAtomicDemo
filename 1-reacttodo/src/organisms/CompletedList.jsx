41c479939392fe03b83652687083ee74af41e4f0
sabb12 <sabb0923@gmail.com>
2024-07-21 22:14:10 +0900

    fixed CompletedList css

--- 1-reacttodo/src/organisms/CompletedList.jsx
+++ 1-reacttodo/src/organisms/CompletedList.jsx
@@ -6,21 +6,23 @@ import ToDoItem from "../molecules/ToDoItem";
 
 export default function CompletedList({ items, onDelete, onToggle }) {
   return (
-    <div>
+    <div className="completedContainer">
       완료 한일
-      {items
-        .filter((item) => item.completed)
-        .map((item) => (
-          <ToDoItem
-            key={item.id}
-            id={item.id}
-            text={item.text}
-            completed={item.completed}
-            onUpdate={() => {}}
-            onDelete={onDelete}
-            onToggle={onToggle}
-          />
-        ))}
+      <div className="completedList">
+        {items
+          .filter((item) => item.completed)
+          .map((item) => (
+            <ToDoItem
+              key={item.id}
+              id={item.id}
+              text={item.text}
+              completed={item.completed}
+              onUpdate={() => {}}
+              onDelete={onDelete}
+              onToggle={onToggle}
+            />
+          ))}
+      </div>
     </div>
   );
 }
